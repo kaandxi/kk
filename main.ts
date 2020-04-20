c627e647203a29b9cf086435de0461d099c50c92
kaandxi <49085820+kaandxi@users.noreply.github.com>
2020-04-21 01:56:46 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,111 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping,
+    走路,
+    停止动画
+}
+function doSomething () {
+    向左动作 = animation.createAnimation(ActionKind.走路, 200)
+    向左动作.addAnimationFrame(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. f f e 2 f f f f f f 2 e f f . 
+. f f f f f e e e e f f f f f . 
+. . f e f b f 4 4 f b f e f . . 
+. . f e 4 1 f d d f 1 4 e f . . 
+. . . f e 4 d d d d 4 e f e . . 
+. . f e f 2 2 2 2 e d d 4 e . . 
+. . e 4 f 2 2 2 2 e d d e . . . 
+. . . . f 4 4 5 5 f e e . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . f f f . . . . . . . . . 
+`)
+    向左动作.addAnimationFrame(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f e e 2 2 2 2 2 2 e f f . . 
+. f f e 2 f f f f f f 2 e f f . 
+. f f f f f e e e e f f f f f . 
+. . f e f b f 4 4 f b f e f . . 
+. . f e 4 1 f d d f 1 4 e f . . 
+. . e f e 4 d d d d 4 e f . . . 
+. . e 4 d d e 2 2 2 2 f e f . . 
+. . . e d d e 2 2 2 2 f 4 e . . 
+. . . . e e f 5 5 4 4 f . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . . . . f f f . . . . 
+`)
+    animation.attachAnimation(精灵, 向左动作)
+}
+function doSomething2 () {
+    停止动作 = animation.createAnimation(ActionKind.Idle, 200)
+    停止动作.addAnimationFrame(img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`)
+    animation.attachAnimation(精灵, 停止动作)
+}
+let 停止动作: animation.Animation = null
+let 向左动作: animation.Animation = null
+let 精灵: Sprite = null
+scene.setBackgroundColor(9)
+精灵 = sprites.create(img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`, SpriteKind.Player)
+精灵.setPosition(90, 90)
+精灵.setFlag(SpriteFlag.StayInScreen, true)
+doSomething()
+doSomething2()
+精灵.ay = 200
+game.onUpdate(function () {
+    if (controller.A.isPressed()) {
+        精灵.vy = -50
+    }
+    if (controller.left.isPressed()) {
+        精灵.vx = -50
+        animation.setAction(精灵, ActionKind.走路)
+    } else if (controller.right.isPressed()) {
+        精灵.vx = 50
+        animation.setAction(精灵, ActionKind.走路)
+    } else {
+        精灵.vx = 0
+        animation.setAction(精灵, ActionKind.Idle)
+    }
+})
